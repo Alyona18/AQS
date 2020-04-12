534933faa9e2ed7559e37faed53d8c3aebe9aade
Alyona18 <47978177+Alyona18@users.noreply.github.com>
2020-04-12 23:50:55 +0600

    Add files via upload

---
+++ AntiQS/ConsoleApplication20.cpp
@@ -0,0 +1,37 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+extern int c = 0;
+
+using namespace std;
+
+void antiqs(int N, int *a) {
+	for (int i = 0; i < N-1 ; i++) {
+		swap(a[i], a[i / 2]);
+	};
+}
+
+
+int main()
+{
+
+	ifstream fin("input.txt");
+	int N;
+	fin >> N;
+	fin.close();
+	int *A;
+	A = new int[N];
+	for (int i=0; i < N; i++) {
+		A[i] = i + 1;
+	};
+	antiqs(N, A);    
+	ofstream fout("output.txt");
+	for (int i = 0; i < N; i++) {
+		fout << A[i]<< endl;
+	};
+	fout.close();
+	return 0;
+}
